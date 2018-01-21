25242b894d18bf04bc643a6f70ea885ff375fac1
Masaki Sato <mustang.97ms@gmail.com>
2018-01-21 16:02:40 +0900

    commit

--- callback.php
+++ callback.php
@@ -1,39 +1,126 @@
 <?php
 
-$access_token = 'TJVUN38VNBVVQz3+EuFnkjdQq5y9tg8Skt0BI+81MFRJSFIKPWQKBjIIxD0gnlGjHK1wpavdGygVOD42d0XRwHxeCjWdihu2NKuy9i9A3simjxwZE+dyu1/illYqxplL57XOfb6dnM83NZVfW6KdhQdB04t89/1O/w1cDnyilFU=';
+$accessToken = 'TJVUN38VNBVVQz3+EuFnkjdQq5y9tg8Skt0BI+81MFRJSFIKPWQKBjIIxD0gnlGjHK1wpavdGygVOD42d0XRwHxeCjWdihu2NKuy9i9A3simjxwZE+dyu1/illYqxplL57XOfb6dnM83NZVfW6KdhQdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v2/bot/message/reply';
+$jsonString = file_get_contents('php://input');
+error_log($jsonString);
+$jsonObj = json_decode($jsonString);
 
-// receive json data from line webhook
-$raw = file_get_contents('php://input');
-$receive = json_decode($raw, true);
+$message = $jsonObj->{"events"}[0]->{"message"};
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 
-// parse received events
-$event = $receive['events'][0];
-$reply_token  = $event['replyToken'];
-$message_text = $event['message']['text'];
+// 送られてきたメッセージの中身からレスポンスのタイプを選択
+if ($message->{"text"} == '確認') {
+    // 確認ダイアログタイプ
+    $messageData = [
+        'type' => 'template',
+        'altText' => '確認ダイアログ',
+        'template' => [
+            'type' => 'confirm',
+            'text' => '元気ですかー？',
+            'actions' => [
+                [
+                    'type' => 'message',
+                    'label' => '元気です',
+                    'text' => '元気です'
+                ],
+                [
+                    'type' => 'message',
+                    'label' => 'まあまあです',
+                    'text' => 'まあまあです'
+                ],
+            ]
+        ]
+    ];
+} elseif ($message->{"text"} == 'ボタン') {
+    // ボタンタイプ
+    $messageData = [
+        'type' => 'template',
+        'altText' => 'ボタン',
+        'template' => [
+            'type' => 'buttons',
+            'title' => 'タイトルです',
+            'text' => '選択してね',
+            'actions' => [
+                [
+                    'type' => 'postback',
+                    'label' => 'webhookにpost送信',
+                    'data' => 'value'
+                ],
+                [
+                    'type' => 'uri',
+                    'label' => 'googleへ移動',
+                    'uri' => 'https://google.com'
+                ]
+            ]
+        ]
+    ];
+} elseif ($message->{"text"} == 'カルーセル') {
+    // カルーセルタイプ
+    $messageData = [
+        'type' => 'template',
+        'altText' => 'カルーセル',
+        'template' => [
+            'type' => 'carousel',
+            'columns' => [
+                [
+                    'title' => 'カルーセル1',
+                    'text' => 'カルーセル1です',
+                    'actions' => [
+                        [
+                            'type' => 'postback',
+                            'label' => 'webhookにpost送信',
+                            'data' => 'value'
+                        ],
+                        [
+                            'type' => 'uri',
+                            'label' => '美容の口コミ広場を見る',
+                            'uri' => 'http://clinic.e-kuchikomi.info/'
+                        ]
+                    ]
+                ],
+                [
+                    'title' => 'カルーセル2',
+                    'text' => 'カルーセル2です',
+                    'actions' => [
+                        [
+                            'type' => 'postback',
+                            'label' => 'webhookにpost送信',
+                            'data' => 'value'
+                        ],
+                        [
+                            'type' => 'uri',
+                            'label' => '女美会を見る',
+                            'uri' => 'https://jobikai.com/'
+                        ]
+                    ]
+                ],
+            ]
+        ]
+    ];
+} else {
+    // それ以外は送られてきたテキストをオウム返し
+    $messageData = [
+        'type' => 'text',
+        'text' => $message->{"text"}
+    ];
+}
 
+$response = [
+    'replyToken' => $replyToken,
+    'messages' => [$messageData]
+];
+error_log(json_encode($response));
 
-// build request headers
-$headers = array('Content-Type: application/json',
-                 'Authorization: Bearer ' . $access_token);
-
-// build request body
-$message = array('type' => 'text',
-                 'text' => $message_text);
-
-$body = json_encode(array('replyToken' => $reply_token,
-                          'messages'   => array($message)));
-
-
-// post json with curl
-$options = array(CURLOPT_URL            => $url,
-                 CURLOPT_CUSTOMREQUEST  => 'POST',
-                 CURLOPT_RETURNTRANSFER => true,
-                 CURLOPT_HTTPHEADER     => $headers,
-                 CURLOPT_POSTFIELDS     => $body);
-
-$curl = curl_init();
-curl_setopt_array($curl, $options);
-curl_exec($curl);
-curl_close($curl);
+$ch = curl_init('https://api.line.me/v2/bot/message/reply');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json; charser=UTF-8',
+    'Authorization: Bearer ' . $accessToken
+));
+$result = curl_exec($ch);
+error_log($result);
+curl_close($ch);
