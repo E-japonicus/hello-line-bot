e06896a8a10d5563fd676e7eb44d267ea54436ee
Masaki Sato <mustang.97ms@gmail.com>
2018-01-21 16:09:58 +0900

    commit

--- callback.php
+++ callback.php
@@ -2,6 +2,7 @@
 
 $accessToken = 'TJVUN38VNBVVQz3+EuFnkjdQq5y9tg8Skt0BI+81MFRJSFIKPWQKBjIIxD0gnlGjHK1wpavdGygVOD42d0XRwHxeCjWdihu2NKuy9i9A3simjxwZE+dyu1/illYqxplL57XOfb6dnM83NZVfW6KdhQdB04t89/1O/w1cDnyilFU=';
 
+
 $jsonString = file_get_contents('php://input');
 error_log($jsonString);
 $jsonObj = json_decode($jsonString);
