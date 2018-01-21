2de53d522aee7c87e5a251915622f9a95162bc8c
Masaki Sato <mustang.97ms@gmail.com>
2018-01-21 15:56:49 +0900

    commit

---
+++ callback.php
@@ -0,0 +1,39 @@
+<?php
+
+$access_token = "TJVUN38VNBVVQz3+EuFnkjdQq5y9tg8Skt0BI+81MFRJSFIKPWQKBjIIxD0gnlGjHK1wpavdGygVOD42d0XRwHxeCjWdihu2NKuy9i9A3simjxwZE+dyu1/illYqxplL57XOfb6dnM83NZVfW6KdhQdB04t89/1O/w1cDnyilFU=";
+
+$url = 'https://api.line.me/v2/bot/message/reply';
+
+// receive json data from line webhook
+$raw = file_get_contents('php://input');
+$receive = json_decode($raw, true);
+
+// parse received events
+$event = $receive['events'][0];
+$reply_token  = $event['replyToken'];
+$message_text = $event['message']['text'];
+
+
+// build request headers
+$headers = array('Content-Type: application/json',
+                 'Authorization: Bearer ' . $access_token);
+
+// build request body
+$message = array('type' => 'text',
+                 'text' => $message_text);
+
+$body = json_encode(array('replyToken' => $reply_token,
+                          'messages'   => array($message)));
+
+
+// post json with curl
+$options = array(CURLOPT_URL            => $url,
+                 CURLOPT_CUSTOMREQUEST  => 'POST',
+                 CURLOPT_RETURNTRANSFER => true,
+                 CURLOPT_HTTPHEADER     => $headers,
+                 CURLOPT_POSTFIELDS     => $body);
+
+$curl = curl_init();
+curl_setopt_array($curl, $options);
+curl_exec($curl);
+curl_close($curl);
